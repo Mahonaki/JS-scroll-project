5c3c32164b710641e729978bde192c33f881aa64
Mahonaki <mahonaki.it@gmail.com>
2024-01-11 09:46:33 +0000

    completed

--- app.js
+++ app.js
@@ -56,6 +56,21 @@ scrollLinks.forEach(function(link) {
         // navigate to specific spot 
         const id = e.currentTarget.getAttribute('href').slice(1);
         const element = document.getElementById(id);
-        
+        // calculate the heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains('fixed_nav');
+        let position = element.offsetTop - navHeight;
+        if(!fixedNav) {
+            position = position - navHeight;
+        }
+        if(navHeight > 82) {
+            position = position + containerHeight;
+        }
+        window.scrollTo({
+            left:0,
+            top:position,
+        });
+        linksContainer.style.height = 0;
     });
 });
